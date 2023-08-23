add8adbf3c2dfc386b1d2af45abf16cca8f40599
Minsang Kim <minsangk@me.com>
2023-08-23 11:37:04 +0900

    [example/pp1] 튜토리얼에 맞춰 코드 정리

--- examples/pp1/web/src/App.tsx
+++ examples/pp1/web/src/App.tsx
@@ -1,22 +1,10 @@
-import "./App.css";
-import { swrFetcher } from "./services/sonamu.shared";
-import { SWRConfig } from "swr";
-import BrandsList from "./BrandsList";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <SWRConfig
-      value={{
-        errorRetryInterval: 3000,
-        errorRetryCount: 3,
-        fetcher: swrFetcher,
-        revalidateOnFocus: true,
-      }}
-    >
-      <div className="App">
-        <BrandsList />
-      </div>
-    </SWRConfig>
+    <div className="app">
+      <Outlet />
+    </div>
   );
 }
 
--- examples/pp1/web/src/main.tsx
+++ examples/pp1/web/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { loadDynamicRoutes } from "@sonamu-kit/react-sui";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SWRConfig } from "swr";
+import PublicIndexPage from "./pages";
+import { swrFetcher } from "./services/sonamu.shared";
+import "semantic-ui-css/semantic.min.css";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <SWRConfig
+    value={{
+      errorRetryInterval: 3000,
+      errorRetryCount: 3,
+      fetcher: swrFetcher,
+      revalidateOnFocus: true,
+    }}
+  >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {loadDynamicRoutes(import.meta.glob("./pages/**/*.tsx"))}
+        </Route>
+        <Route path="*" element={<div>404 Page Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  </SWRConfig>
+);
