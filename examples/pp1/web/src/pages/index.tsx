3c845b7df0c12567f2127cf826ac6670da4c76d1
Minsang Kim <minsangk@me.com>
2023-08-23 15:50:39 +0900

    [examples/pp1] index.tsx 추가

---
+++ examples/pp1/web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type PublicIndexPageProps = {};
+export default function PublicIndexPage(props: PublicIndexPageProps) {
+  return (
+    <div className="intro">
+      <h1>🌲</h1>
+      <h2>Hello, Sonamu!</h2>
+      <div style={{ marginTop: "3em", color: "#666" }}>
+        <strong>Sonamu</strong> is the full-stack framework for startup
+        developers,
+        <br />
+        based on <strong>Node.js/TypeScript</strong>.
+      </div>
+      {/* <BrandsList /> */}
+    </div>
+  );
+}
