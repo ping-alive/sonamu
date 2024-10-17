4dbf142157e4573538e1d7557cdd277e2957d0f5
Minsang Kim <minsangk@me.com>
2024-10-17 16:05:45 +0900

    [sonamu] 0.2.50 빌드 배포 (CJS/ESM 듀얼 빌드)

--- modules/sonamu/tsup.config.js
+++ modules/sonamu/tsup.config.js
@@ -4,10 +4,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/bin/cli.ts", "src/bin/cli-wrapper.ts"],
   dts: true,
-  format: [
-    // "esm",
-    "cjs",
-  ],
+  format: ["esm", "cjs"],
   target: "es2020",
   clean: true,
   sourcemap: true,
